c1bdb13d6c9736066a54f2546755ead3943cdd0c
Greenball233 <3120393927@qq.com>
2023-01-19 21:40:23 +0800

    1111

--- sus-io/process.py
+++ sus-io/process.py
@@ -2,9 +2,9 @@ import sys
 import sus
 
 if __name__ == "__main__":
-    if len(sys.argv) > 1:
+    if len(sys.argv) > 2:
         print("Try to transform from sus to json")
-        with open(sys.argv[0], "r") as fi, open(sys.argv[1], "w") as fo:
+        with open(sys.argv[1], "r") as fi, open(sys.argv[2], "w") as fo:
             try:
                 score = sus.load(fi)
                 json = score.to_json()
